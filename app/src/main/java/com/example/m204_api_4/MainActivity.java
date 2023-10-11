490e9604dd6c5b52b9984011602f254d08874e8c
unknown <ouardijamal87@gmail.com>
2023-10-11 09:17:18 +0100

    msg

---
+++ app/src/main/java/com/example/m204_api_4/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.m204_api_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
